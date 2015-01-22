99ad23655b8c8a3a12b6c6c534e3073235a12bbd
vagrant <vagrant@precise32.(none)>
2015-01-22 21:40:25 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,18 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Hey there!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+
+function projectClick(e) { 
+    console.log("Project clicked");
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
 }
